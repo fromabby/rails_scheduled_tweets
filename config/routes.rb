2e0908f130855ec82e2fcc2d2f4179dc56423382
Abby Bonuan <abbonuan@gmail.com>
2023-05-29 16:46:50 +0800

    add update password

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,13 @@ Rails.application.routes.draw do
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
-  delete "logout", to: "sessions#destroy"
-
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+  
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
+  
+  delete "logout", to: "sessions#destroy"
 
   # alternate to root: get "/"
   root to: "main#index"
