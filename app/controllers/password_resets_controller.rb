4b067b4ba634e84e77226d9c0273a7c5d3f3839c
Abby Bonuan <60252891+fromabby@users.noreply.github.com>
2023-05-30 02:28:25 +0800

    add forgot password form and controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -21,4 +21,29 @@ class PasswordResetsController < ApplicationController
     end
     redirect_to root_path, notice: "Check your email for the reset password link"
   end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "Your token has expired"
+
+    if @user.present?
+    end
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to root_path, notice: "Your password was reset successfully."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
